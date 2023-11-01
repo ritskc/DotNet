9249946e0df18b5d819a82dbe6f758b2bed31358
Ritesh Charkhawala <rits.kc@gmail.com>
2023-11-01 15:41:03 -0400

    Attributes. Attributes allow you to add declarative information to your program. This information can then be queried at runtime using reflection

---
+++ Attributes/Calculator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet.Attributes
+{
+    public class Calculator
+    {
+        [Obsolete("Use Add(List<int> numbers)")]
+        public static int Add(int number1, int number2)
+        {
+            return number1 + number2;
+        }
+
+        public static int Add(List<int> numbers)
+        {
+            return numbers.Sum(x => x);
+        }
+    }
+}
