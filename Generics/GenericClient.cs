ad3b6086b97dbf2c92688ec774d12a4b43aea510
Ritesh Charkhawala <rits.kc@gmail.com>
2023-11-02 18:53:18 -0400

    Generics

---
+++ Generics/GenericClient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet.Generics
+{
+    public class GenericClient
+    {
+        public void Get()
+        {
+            Calculator<int>.AreEqual(1, 1);
+        }
+    }
+}
