3049485e42e084090c0659a984482e2991866d74
Ritesh Charkhawala <rits.kc@gmail.com>
2023-11-03 10:04:58 -0400

    Extenstion Method Class should be static & type of extension method type should be passed as first variable as this keyword

---
+++ ExtensionMethod/StringHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet.ExtensionMethod
+{
+    public static class StringHelper
+    {
+        public static string ChangeFirstLetterCase(this string inputString)
+        {
+            if (inputString.Length > 0)
+            {
+                char c = inputString[0];
+
+                inputString = inputString.Remove(0, 1);
+                return char.ToUpper(c) + inputString;
+            }
+            return inputString;
+        }
+    }
+}
